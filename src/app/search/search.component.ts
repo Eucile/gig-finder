ce24435b8f8ce5143471cdfc1285f8c6e8339764
Student <example@example.com>
2018-11-01 10:19:30 -0700

    add conditional to sanitized url injector

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -30,8 +30,8 @@ export class SearchComponent  {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
-
   getConcerts() {
+
     this.location =(<HTMLInputElement> document.getElementById('locationSearch')).value;
     this.artist =(<HTMLInputElement> document.getElementById('artistSearch')).value;
     this.startDate = (<HTMLInputElement> document.getElementById('startDate')).value;
@@ -46,7 +46,6 @@ export class SearchComponent  {
         this.dateRange = this.startDate + "00" + "-" + this.endDate + "00";
       }
     }
-    // const test = this.concertService.onClickMe(this.location, this.artist, this.dateRange);
 
     const test = this.concertService.onClickMe(this.location, this.artist, this.startDate, this.endDate);
 
@@ -54,53 +53,34 @@ export class SearchComponent  {
       this.showSpin = false;
       let body = JSON.parse(response.toString());
 
-      // body.events.event.forEach((event, i) => {
-
       body._embedded.events.forEach((events, i) => {
 
-        // this.artistsList.push(
-        //   new Result(event.title, event.venue_address, event.city_name, event.region_abbr, event.postal_code, event.start_time)
-        // );
-
         this.artistsList.push(
-            new Result(events.name, events._embedded.venues[0].name, events._embedded.venues[0].address.line1, events._embedded.venues[0].city.name, events._embedded.venues[0].state.stateCode, events._embedded.attractions[0].name, events.start_time, "")
-          );
+          new Result(events.name, events._embedded.venues[0].name, events._embedded.venues[0].address.line1, events._embedded.venues[0].city.name, events._embedded.venues[0].state.stateCode, events._embedded.attractions[0].name, events.start_time, "", false)
+        );
 
         this.spotifyService.getToken().subscribe(tokenOne => {
           this.spotifyService.searchMusic(tokenOne.access_token, events._embedded.attractions[0].name).subscribe(artistInfo => {
-            this.spotifyService.getToken().subscribe(tokenTwo => {
-              if (artistInfo.artists.items[0] != undefined) {
+            if (artistInfo.artists.items[0] != undefined) {
+              this.spotifyService.getToken().subscribe(tokenTwo => {
                 this.spotifyService.searchAlbum(tokenTwo.access_token, artistInfo.artists.items[0].id).subscribe(artistID => {
 
-                  this.artistsList[i].albumId = "https://open.spotify.com/embed/album/" + artistID.items[0].id;
-
-                 // this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl("https://open.spotify.com/embed/album/" + artistID.items[0].id);
-
-                  // console.log(this.safeUrl);
-
-                  console.log(this.artistsList[i]);
-
-                  // this.artistsList = artistID.items[0].id;
-                  // console.log(this.artistsList.title)
-
-                  console.log(this.artistsList);
-
+                  if(this.artistsList) {
+                    this.artistsList[i].exist = true;
+                    this.artistsList[i].albumId = "https://open.spotify.com/embed/album/" + artistID.items[0].id;
+                  } else {
+                    this.artistsList[i].exist = false;
+                  }
                 });
-              } else {
-                this.artistsList[i].albumId = 'We did it';
-              }
-            });
+              });
+            } else {
+              this.artistsList[i].albumId = 'null';
+            }
           });
         });
       })
-      // .then(() => {
-      //   console.log(this.albumIDs)
-      // });
-      console.log(this.albumIDs);
     }, function(error) {
       console.log(error)
-
-
     });
   }
 }
